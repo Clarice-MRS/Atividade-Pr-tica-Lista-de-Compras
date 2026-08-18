17c9cb38c7d7ee9a3fc851cc6b8cfe48a06e77ab
Clarice-MRS <claricerdg461@gmail.com>
2026-08-17 22:01:43 -0300

    FEAT: remover item

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function salvarItem() {}
+const form = document.getElementById("form");
 
-function editarItem() {}
 
-function removerItem() {}
+function criarItem() {}
+
+function editarItem(id) {
+    const item = document.getElementById(`item-${id}`);
+}
+
+function removerItem(id) {
+    console.log(id);
+    const item = document.getElementById(`item-${id}`);
+    console.log(item);
+    if (confirm("Remover item?"))
+        item.remove();
+}
