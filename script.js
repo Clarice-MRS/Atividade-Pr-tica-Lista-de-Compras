e648dac14e456b1752f0c75b793c9b962e70b43b
brensant <breno_santana_silva@hotmail.com>
2026-08-21 12:19:44 -0300

    FIX: criar item

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 const form = document.getElementById("form");
 const nomeInput = document.getElementById("nome-input");
 const listaCompras = document.querySelector("#lista");
+const botaoAdicionar = document.querySelector('button[type="submit"]');
+
+let totalItems = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+    botaoAdicionar.addEventListener("click", criarItem(evento));
+})
 
 function criarItem(evento) {
-
     evento.preventDefault();
 
-    const valor = nomeInput.value;
+    totalItems += 1;
 
+    const valor = nomeInput.value;
     const elemento = document.createElement("li");
 
+    elemento.classList.add("Lista-item");
+    elemento.id = `item-${totalItems}`;
     elemento.innerHTML = `
-        <span class="titulo-item">${valor}</span>
-
-        <button class="Lista-editar">
+        <input type="checkbox" name="status" id="status-input" class="Lista-checkbox">
+        <span class="Lista-texto">${valor}</span>
+        <button class="Lista-editar" onclick="editarItem(${totalItems})">
             <img src="img/pencil.svg" alt="editar">
         </button>
-
-        <button class="Lista-remover">
+        <button class="Lista-editar" onclick="removerItem(${totalItems})">
             <img src="img/trash.svg" alt="remover">
         </button>
     `;
@@ -27,9 +35,9 @@ function criarItem(evento) {
     document.getElementById('msg-vazio').style.display = 'none';
 
     document.getElementById("form").reset();
-    
-	// Remover min-height do style da .Lista caso exista
-	// Arrumar botoes editar e remover
+
+    // Remover min-height do style da .Lista caso exista
+    // Arrumar botoes editar e remover
 }
 
 form.addEventListener("submit", criarItem);
@@ -38,12 +46,12 @@ function editarItem(id) {
     const item = document.getElementById(`item-${id}`);
     const nomeItem = item.children[1].textContent;
 
-	const novoNome = prompt(`Editar item "${nomeItem}":`, nomeItem);
+    const novoNome = prompt(`Editar item "${nomeItem}":`, nomeItem);
 
-	const spanNomeItem = item.getElementsByClassName("Lista-texto")[0];
+    const spanNomeItem = item.getElementsByClassName("Lista-texto")[0];
 
-	if (novoNome)
-		spanNomeItem.textContent = novoNome;
+    if (novoNome)
+        spanNomeItem.textContent = novoNome;
 }
 
 function removerItem(id) {
@@ -53,10 +61,10 @@ function removerItem(id) {
     if (confirm(`Remover item "${nomeItem}"?`))
         item.remove();
 
-	const numItens = document.getElementsByClassName("Lista-item").length;
-	const lista = document.getElementById("lista");
-	const pMsgVazio = lista.firstElementChild;
+    const numItens = document.getElementsByClassName("Lista-item").length;
+    const lista = document.getElementById("lista");
+    const pMsgVazio = lista.firstElementChild;
 
-	if (numItens === 0)
-		pMsgVazio.style.display = "block";
+    if (numItens === 0)
+        pMsgVazio.style.display = "block";
 }
